53156e1c31007d214ccf8e19b458e7ef678a8960
Bruno <brpadilha.dev@gmail.com>
2023-04-10 22:56:09 -0300

    cria o gateway in memory para adaptar o teste ao gateway

--- src/lists/lists.service.spec.ts
+++ src/lists/lists.service.spec.ts
@@ -1,26 +1,23 @@
-import { ListModel } from './entities/list.model';
+import { of } from 'rxjs';
+import { ListGatewayInMemory } from './gateways/list-gateway-in-memory';
 import { ListsService } from './lists.service';
 
-const mockList = {
-  create: jest
-    .fn()
-    .mockReturnValue(Promise.resolve(new ListModel({ name: 'my list' }))),
-};
-
 const mockHttpService = {
-  post: jest.fn(),
+  post: jest.fn().mockReturnValue(of(null)),
 };
 
 describe('ListsService', () => {
   let service: ListsService;
+  let listGateway: ListGatewayInMemory;
 
   beforeEach(() => {
-    service = new ListsService(mockList as any, mockHttpService as any);
+    listGateway = new ListGatewayInMemory();
+    service = new ListsService(listGateway as any, mockHttpService as any);
   });
 
   it('should create a list', async () => {
-    const list = await service.create({ name: 'Test' });
-    console.log(list);
+    const list = await service.create({ name: 'my list' });
+    expect(listGateway.items).toEqual([list]);
   });
 
   // let service: ListsService;
